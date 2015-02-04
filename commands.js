699decb1adb7b5fde307e495e6c13f4a12a21f18
Raina4uberz <raina4uberz@gmail.com>
2015-02-04 15:20:52 +0530

    Update commands.js

--- commands.js
+++ commands.js
@@ -2298,4 +2298,44 @@ var commands = exports.commands = {
             }
         }, 1000);
     },
+    regdate: function (target, room, user, connection) {
+        if (!this.canBroadcast()) return;
+        if (!target || target == "." || target == "," || target == "'") return this.parse('/help regdate');
+        var username = target;
+        target = target.replace(/\s+/g, '');
+
+        var options = {
+            host: "www.pokemonshowdown.com",
+            port: 80,
+            path: "/forum/~" + target
+        };
+
+        var content = "";
+        var self = this;
+        var req = http.request(options, function (res) {
+
+            res.setEncoding("utf8");
+            res.on("data", function (chunk) {
+                content += chunk;
+            });
+            res.on("end", function () {
+                content = content.split("<em");
+                if (content[1]) {
+                    content = content[1].split("</p>");
+                    if (content[0]) {
+                        content = content[0].split("</em>");
+                        if (content[1]) {
+                            regdate = content[1];
+                            data = username + ' was registered on' + regdate + '.';
+                        }
+                    }
+                } else {
+                    data = username + ' is not registered.';
+                }
+                self.sendReplyBox(data);
+                room.update();
+            });
+        });
+        req.end();
+    },
 };
